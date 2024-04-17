6453abc053bcf2d9a1c0eaed2c8dc2187b488f36
pravesh meena <pravesh.meena2892@gmail.com>
2024-04-17 08:36:04 +0530

    final update

--- script.js
+++ script.js
@@ -1,36 +1,19 @@
-// Inputs
+
 const inputs = document.querySelectorAll("input, select");
-// Error icons
 const errorIcons = document.querySelectorAll(".bi-exclamation-circle");
-// Error texts
 const errorTexts = document.querySelectorAll(
   ".bi-exclamation-circle .tooltiptext",
 );
 
-// Age group input
 const ageGroup = document.querySelector("#age-group");
-
-// Overall income element
 const overallIncome = document.querySelector("#overall-income");
-// Modal
 const resultDiv = document.querySelector("#result");
-// Close button
 const closeButton = document.querySelector(".close");
-// Submit button
 const submitButton = document.querySelector(".submit");
-// Tax text
 const taxText = document.querySelector("#tax-text");
-
-//overall income value
 let overAllIncome = 0;
-
-// Income limit
 const incomeLimit = 800000;
 
-// Functions
-
-// Input checker function
-// This function checks if the values in input and select field valid or not and show error icon accordingly
 const inputChecker = (input, errorIcon, errorText) => {
   if (input.value.length == 0) {
     errorIcon.style.display = "flex";
@@ -46,8 +29,6 @@ const inputChecker = (input, errorIcon, errorText) => {
   }
 };
 
-// Submit checker function
-// This function checks for error after submit button calls this function, it gives true if all inputs are valid and error free
 const submitChecker = () => {
   let checkedInputs = Array.from(inputs).filter((inp) =>
     inp === ageGroup
@@ -58,8 +39,7 @@ const submitChecker = () => {
   return checkedInputs.length === inputs.length;
 };
 
-// Tax calculator function
-// Calculates the overall income after tax calculations
+
 const taxCalculator = () => {
   let incomeAfterDeductions =
     Number(inputs[0].value) + Number(inputs[1].value) - Number(inputs[3].value);
@@ -75,8 +55,7 @@ const taxCalculator = () => {
       : "after tax deductions";
 };
 
-// Taxed income calculator function
-// Calculates tax amounts which needs to be deducted
+
 const taxedIncomeCalculator = (incomeAfterDeductions) => {
   let taxableIncome = incomeAfterDeductions - incomeLimit;
   let taxAmount =
@@ -88,14 +67,12 @@ const taxedIncomeCalculator = (incomeAfterDeductions) => {
   return incomeAfterDeductions - taxAmount;
 };
 
-// Event listeners
 
-// Hide the modal if close button is clicked
 closeButton.addEventListener("click", () => {
   resultDiv.style.display = "none";
 });
 
-// Checks every event for events focus and change and call inputChecker function to verify valid inputs and show error if invalid inputs
+
 inputs.forEach((input, index) => {
   input.addEventListener("change", () => {
     inputChecker(input, errorIcons[index], errorTexts[index]);
@@ -105,7 +82,7 @@ inputs.forEach((input, index) => {
   });
 });
 
-// Shows modal on click of submit button if all inputs are valid. Calls tax calculator function for overall income. Show error icons if inputs are invalid
+
 submitButton.addEventListener("click", () => {
   if (submitChecker()) {
     taxCalculator();
